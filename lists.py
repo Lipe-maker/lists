75121c113a7018a7a5ed0a55dc09b9c9da38f81b
Lipe-maker <bottidupratfelipe@gmail.com>
2024-09-03 18:06:15 +0200

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,22 @@
+names=['rafael','jack','leon','thomas','shelly']
+
+print (names [0:4])
+print (names [2])
+
+
+del names[3]
+
+#insert [0,'paul']
+
+
+#names.remove['jack']
+
+#names.pop[1]
+
+#names.[4]=beasechurger
+
+#names.append('sandwich')
+
+#names.clear
+
+print(names)
