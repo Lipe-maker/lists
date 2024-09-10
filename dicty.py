8148651d3ea9f50c28fb5f6fbbf3739676fd3c24
Lipe-maker <bottidupratfelipe@gmail.com>
2024-09-10 17:58:55 +0200

    Create dicty.py

---
+++ dicty.py
@@ -0,0 +1,15 @@
+dict {
+    'age';'11'
+    'name';'Felipe'
+    'height';'1,55'
+    'weight';'40kg'
+    'body type';'skinny'
+}
+print(dict['height'])
+print(dict.keys())
+print(dict.values())
+print(dict.items())
+dict['name']='eren'
+dict['color']='green'
+dict.update({'age':15})
+dict.update({'color':'green'})
