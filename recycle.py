7c33c158a5faf9c85a134dc77c7371a6ef6458b1
Lipe-maker <bottidupratfelipe@gmail.com>
2024-12-03 17:58:28 +0100

    Update recycle.py
    yeeeee2.0

--- recycle.py
+++ recycle.py
@@ -1,7 +1,7 @@
 import pgzrun
 import random
 
-FONT_option - (255,255,255)
+FONT_option = (255,255,255)
 WIDTH = 800
 HEIGHT = 600
 CENTRE_X = WIDTH / 2
@@ -32,20 +32,21 @@ def draw():
 
 def update():
     global items
-    if len(items) -- 0:
-        items - make_items(current_level)
+    if len(items) == 0:
+        items = make_items(current_level)
 
 def make_items(number_of_extra_items):
-    items_to_create - get_option_to_create(number_of_extra_items)
+    items_to_create = get_option_to_create(number_of_extra_items)
     new_items = create_items(items_to_create)
     layout_items(new_items)
     animate_items(new_items)
     return new_items
 
-def get_option_to_create_(number_of_extra_items):
+def get_option_to_create(number_of_extra_items):
     items_to_create = ['paper']
     for i in range(0, number_of_extra_items):
-        random_option = random.choice(ITEMS)
+        option = random.choice(ITEMS)
+        items_to_create.append(option)
     return items_to_create
 
 def create_items(items_to_create):
@@ -59,6 +60,50 @@ def layout_items(items_to_layout):
     number_of_gaps = len(items_to_layout) + 1
     gap_size = WIDTH / number_of_gaps
     random.shuffle(items_to_layout)
-    for index, item in enumarate(items_to_layout):
+    for index, item in enumerate(items_to_layout):
         new_x_pos = (index + 1 ) * gap_size
         item.x = new_x_pos
+
+def animate_items(items_to_animate):
+    global animations
+    for item in items_to_animate:
+        duration = START_SPEED - current_level
+        item.anchor = ('center','bottom')
+        animation = animate(item, duration=duration, on_finished=handle_game_over, y=HEIGHT)
+        animations.append(animation)
+
+def handle_game_over():
+    global game_over
+    game_over = True
+
+ 
+def on_mouse_down(pos):
+    global items, current_level
+    for item in items:
+        if item.collidepoint(pos):
+            if 'paper' in item.image:
+                handle_game_complete()
+            else:
+                handle_game_over
+
+def handle_game_complete():
+    global current_level, items, animations, game_complete
+    stop_animations(animations)
+    if current_level == FINAL_LEVEL:
+        game_complete = True
+    else:
+        current_level+=1
+        items = []
+        animations = []
+
+def stop_animations(animations_to_stop):
+    for animation in animations_to_stop:
+        if animation.running:
+            animation.stop()
+
+def display_message(heading_text, sub_heding_text):
+    screen.draw.text(heading_text, fontsize=60, center=CENTRE, color='white')
+    screen.draw.text(sub_heding_text, fontsize=30, center=(CENTRE_X, CENTRE_Y + 30), color='white')
+
+
+pgzrun.go()
