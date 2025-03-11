c0c55c8ff81dcd274d2b68d05a617a70236587b6
Lipe-maker <bottidupratfelipe@gmail.com>
2025-03-11 17:56:00 +0100

    the best birthday EVER

---
+++ bday.py
@@ -0,0 +1,27 @@
+import pygame
+import time
+
+pygame.init()
+
+WIDTH = 600
+HEIGHT = 600
+
+display_surface=pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption('Birthday Greeting Card')
+
+img=pygame.image.load('bday card.jpg')
+image = pygame.transform.scale(img, (WIDTH,HEIGHT))
+
+
+while(True):
+    font=pygame.font.SysFont('Times New Roman',72)
+    text=font.render('Happy',True,(0,0,0))
+    text2=font.render('Birthday',True,(0,0,0))
+    display_surface.fill((255,255,255))
+    display_surface.blit(image,(0,0))
+    display_surface.blit(text,(210,180))
+    display_surface.blit(text2,(180,264))
+    pygame.display.update()
+    time.sleep(2)
+
+    image2=pygame.image.load('birthday-card2.jpg')
